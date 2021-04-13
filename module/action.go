b8b04abcc26e39ca4bdaf48a87e0b9031167675a
Javad <ja7adr@gmail.com>
2021-04-13 15:16:04 +0430

    Actions Interface implemented

--- module/action.go
+++ module/action.go
@@ -1,4 +1,29 @@
 package module
 
+import "github.com/Ja7ad/NipoCli/cmd"
+
 type Actions interface {
+	ReadLine() string
+	ReadLineErr() (string, error)
+
+	Println(val ...interface{})
+	Print(val ...interface{})
+	Printf(format string, val ...interface{})
+	ShowPagedOut(text string) error
+	SetPrompt(promote string)
+	SetMultiPrompt(promote string)
+	ShowPrompt(show bool)
+	Commands() []*cmd.CMD
+	HelpInfo() string
+	ClearScreen() error
+	Stop()
+}
+
+type ShellActions struct {
+	*Shell
+}
+
+// ReadLine read a line from output
+func (s *ShellActions) ReadLine() string {
+
 }
