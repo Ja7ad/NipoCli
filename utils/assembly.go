be2cff8c0ea79bffcf67f823fec5b9e7cf831c53
Javad <ja7adr@gmail.com>
2021-04-13 10:25:49 +0430

    Nipo Assembly information

---
+++ utils/assembly.go
@@ -0,0 +1,6 @@
+package utils
+
+// NipoCli Assembly Info
+var NipoVer string = "0.0.1"
+var Desc string = "Nipo is a powerful, fast, multi-thread, clustered and in-memory key-value database"
+var Web string = "nipo.dev"
